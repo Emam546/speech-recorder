2c706a20cbaf6a4e4258c43b16916c43c21ce0dc
Emam546 <emam54637@gmail.com>
2024-03-30 01:35:43 +0200

    adding github actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
 
-export default nextConfig;
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+    // trim off `<owner>/`
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+    assetPrefix = `/${repo}/`;
+    basePath = `/${repo}`;
+}
+
+module.exports = {
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+};
