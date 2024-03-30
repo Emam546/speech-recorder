44b2a3783c4bb57ba59677e703c7ceae5abc3fea
Emam546 <emam54637@gmail.com>
2024-03-30 03:20:12 +0200

    try to fix the problem

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,16 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-let assetPrefix = "";
 let basePath = "";
 
 if (isGithubActions) {
     // trim off `<owner>/`
     const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
 
-    assetPrefix = `/${repo}/`;
+
     basePath = `/${repo}`;
 }
 /** @type {import('next').NextConfig} */
 const NextConfig = {
-    assetPrefix: assetPrefix,
     output: "export",
     basePath: basePath,
     images: {
